c09bf5e422905da35fe3ea410a54dff5c5c68d94
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-04-08 13:15:11 +0300

    finish

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const splitStr = sourceString.split(';');
+
+  const filterSpaces = splitStr.map((item) => item.trim());
+
+  const filterEmptyLines = filterSpaces.filter((item) => item.length > 0);
+
+  const keyValue = filterEmptyLines.map((item) => {
+    const parts = item.split(':');
+
+    return [parts[0].trim(), parts[1].trim()];
+  });
+
+  return Object.fromEntries(keyValue);
 }
 
 module.exports = convertToObject;
